cfcee2d35c3e4e8c32980ccd031d8e0ea8396a8c
CarolynnFleming <cfcatpink@gmail.com>
2022-05-03 16:20:44 -0700

    returning info from api to render onto page

--- src/views/Characters/Detail.js
+++ src/views/Characters/Detail.js
@@ -18,5 +18,24 @@ export default function Detail() {
         fetchMovie();
     }, []);
 
-    
+    return (
+    <>
+        <h1>Movie Detail</h1>
+        <Link to="/">Back to Studio Ghibli Movie List</Link>
+        {loading ? (
+            <p>Loading Movie...</p>
+        ) : (
+            <article>
+                <h2>{studioGhibli.title}</h2>
+                <h2>{studioGhibli.original_title}</h2>
+                <img alt={`Image of ${studioGhibli.title}`} src={studioGhibli.image} />
+                <img src={studioGhibli.movie_banner} />
+                <h4>{studioGhibli.description}</h4>
+                <h2>{studioGhibli.director}</h2>
+                <h2>{studioGhibli.release_date}</h2>
+                <p>Characters: {studioGhibli.people}</p>
+
+            </article>
+        )}
+    </>)
 }
