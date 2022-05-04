086b4c91c7a9233be0d6f73bbd4ea24c7fd64e8c
CarolynnFleming <cfcatpink@gmail.com>
2022-05-03 17:04:07 -0700

    returning selector with multiple options for producers

--- src/views/Characters/List.js
+++ src/views/Characters/List.js
@@ -31,4 +31,39 @@ setLoading(false);
     };
     fetchMovies();
 }, [loaction.search]);
+
+return(
+    <>
+    <h1>Movie List</h1>
+    {loading ? (
+        <p>Loading Movies...</p>
+    ) : (
+        <section>
+            <label htmlFor="producer">Film Producer:</label>
+            <select id="producer" value={producer} onChange={handleProducerChange}>
+                <option value="all">All</option>
+                <option value="Isao Takahata">Isao Takahata</option>
+                <option value="Toru Hara">Toru Hara</option>
+                <option valu="Hayao Miyacaki">Hayao Miyazaki</option>
+                <option value="Toshio Suzuki">Toshio Suzuki</option>
+                <option value="Yoshio Nishimura">Yoshio Nishimura</option>
+                <option value="Isao Takahata">Isao Takahata</option>
+                <option value="Vincent Maraval">Vincent Maraval</option>
+                <option value="Pascal Caucheteux">Pascal Caucheteux</option>
+                <option value="Gregoire Sorlat">Gregoire Sorlat</option>
+            </select>
+            {movies.map((movie) => (
+                <article key={movie.id}>
+                    <Link to={`/characters/${movie.id}`}>
+                        <h3>{movie.title}</h3>
+                    </Link>
+                    <p>{movie.original_title}</p>
+                    <p>{movie.director}</p>
+                    <p>Producer: {movie.producer}</p>
+                </article>
+            ))}
+        </section>
+    )}
+    </>
+);
 }
